7b21dacac0e9c5a1513429cd0025cd70c2376b0b
Almond13 <95198435+Almond13@users.noreply.github.com>
2022-02-23 16:32:40 +0900

    Add files via upload

--- js/defalt.js
+++ js/defalt.js
@@ -1,5 +1,15 @@
 let btn = document.querySelector('.btn-close');
-        let popup = document.querySelector('.popup-modal');       
-        btn.addEventListener('click',function(){
-            popup.style.display='none';
-        })
+let popup = document.querySelector('.popup-modal');       
+    btn.addEventListener('click',function(){
+    popup.style.display='none';
+    })
+
+let popupLink = document.querySelectorAll('.popup-link');
+let popupLinkID;
+for(var i = 0; i < popupLink.length; i++){
+    popupLink[i].addEventListener('click', function(){
+      popupLinkID = this.getAttribute('href');
+      document.querySelector(popupLinkID).style.display = 'block';
+    });
+  }
+        
